b91d4ef3222a7cd923a63e1ac4b5784cc5751401
unknown <hussainalirezaie54@gmail.com>
2025-09-19 08:01:32 +0430

    fix the sever js and pakege json

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 import connectDB from "./configs/db.js";
 
 import authRoutes from "./Routes/authRoutes.js";
 import projectRoutes from "./Routes/projectRoutes.js";
 import messageRoutes from "./Routes/messageRoutes.js";
 import homeRoutes from "./Routes/homeRoutes.js"; 
-import skillRoutes from "./Routes/skillRoutes.js"
+import skillRoutes from "./Routes/skillRoutes.js";
 import aboutRoutes from "./Routes/about.js";
 import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 
@@ -25,13 +26,27 @@ app.use(express.urlencoded({ extended: true }));
 // مسیر استاتیک برای نمایش عکس‌ها
 app.use("/uploads", express.static(path.join(path.resolve(), "uploads")));
 
-// Routes
+// API Routes
 app.use("/api/about", aboutRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/home", homeRoutes);  
-app.use("/api/skills", skillRoutes); // ✅ بخش Skills اضافه شد
+app.use("/api/skills", skillRoutes); 
+
+// ----------------------------
+// ✅ سرو کردن فرانت‌اند در حالت production
+// ----------------------------
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+}
 
 // Error Middlewares
 app.use(notFound);
